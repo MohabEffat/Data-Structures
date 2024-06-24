5eb94e95c08654ea4a073574c0575739bbbb5b53
Mohab <Mohabeffat76@gmail.com>
2024-06-24 04:19:32 +0300

    Hash32 FNV-1a

---
+++ keyValuePair - Dictionary/Java/src/Hash.java
@@ -0,0 +1,16 @@
+import java.nio.charset.StandardCharsets;
+
+public class Hash {
+    public void Hash32(String str){
+        final long OFFSET_BASIS = 2166136261L;
+        final long FNV_PRIME = 16777619L;
+        byte[] data = str.getBytes(StandardCharsets.US_ASCII);
+        long hash = OFFSET_BASIS;
+        for (byte b : data) {
+            hash ^= b;
+            hash *= FNV_PRIME;
+            hash &= 0xFFFFFFFFL;
+        }
+        System.out.println(str + ", " + hash + ", " + Long.toHexString(hash) );
+    }
+}
--- keyValuePair - Dictionary/Java/src/Main.java
+++ keyValuePair - Dictionary/Java/src/Main.java
@@ -2,19 +2,19 @@
 public class Main {
     public static void main(String[] args) {
         Dictionary<String, String> dic = new Dictionary<String, String>();
-        dic.Print();
-        dic.Set("Sinar", "Sinar@Gmail.com");
-        dic.Set("Elvis", "Elvis@Gmail.com");
-        dic.Set("Tane", "Tane@Gmail.com");
-        dic.Print();
-        dic.Set("Mohab", "Mohab@Gmail.com");
-        dic.Set("Toqa", "Toqa@Gmail.com");
-        dic.Print();
-        dic.Remove("Sinar");
-        dic.Remove("Elvis");
-        dic.Remove("Tane");
-        dic.Remove("Mohab");
-        dic.Remove("Toqa");
-        dic.Print();
+//        dic.Print();
+//        dic.Set("Sinar", "Sinar@Gmail.com");
+//        dic.Set("Elvis", "Elvis@Gmail.com");
+//        dic.Set("Tane", "Tane@Gmail.com");
+//        dic.Print();
+//        dic.Remove("Sinar");
+//        dic.Remove("Elvis");
+//        dic.Remove("Tane");
+//        dic.Print();
+        System.out.println("------------------------");
+        Hash hash = new Hash();
+        hash.Hash32("this is Original Text");
+        hash.Hash32("This is Original Text");
+
     }
 }
